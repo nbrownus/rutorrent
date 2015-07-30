0251b112749cf6b72f8aa9784da163c7249b20b3
Nate Brown <nbrown.us@gmail.com>
2015-07-29 22:30:11 -0700

    Removing unused plugins

--- plugins/diskspace/conf.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$diskUpdateInterval = 10;	// in seconds
-$notifySpaceLimit = 512;	// in Mb
-$partitionDirectory = &$topDirectory;	// mount point of a torrents partition, $topDirectory by default
